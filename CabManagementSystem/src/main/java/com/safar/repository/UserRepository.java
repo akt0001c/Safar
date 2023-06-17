ed34d4262e0dfeeccb0c55c0e2e48bf3f33683a9
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-06-17 16:21:09 +0530

    delete update all method added

--- CabManagementSystem/src/main/java/com/safar/repository/UserRepository.java
+++ CabManagementSystem/src/main/java/com/safar/repository/UserRepository.java
@@ -4,9 +4,12 @@ import com.safar.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<Users, Integer> {
     public Optional<Users> findByEmail(String email);
+
+    public List<Users> findAllByRole(String role);
 }
