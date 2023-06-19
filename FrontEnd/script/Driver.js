b0474f201d04898013acd5c8eeb540cb885a7769
Susheel Kumar <sushilkumar8081742575@gmail.com>
2023-06-19 13:10:18 +0530

    index updated

--- FrontEnd/script/Driver.js
+++ FrontEnd/script/Driver.js
@@ -9,30 +9,104 @@ let dRate = document.getElementsByClassName("dRate");
 let tbody = document.getElementById("tbody");
 
 
-for(let i = 0; i < 5; i++){
-    display();
+// for(let i = 0; i < 5; i++){
+//     display();
+// }
+
+
+// function display(){
+//     let tr = document.createElement("tr");
+//     let td1 = document.createElement("td");
+//     td1.innerHTML = '1';
+//     let td2 = document.createElement("td");
+//     td2.innerHTML = 'Aman'
+//     let td3 = document.createElement("td");
+//     td3.innerHTML = 'aman@gmail.com';
+//     let td4 = document.createElement("td");
+//     td4.innerHTML = 'Kanpur';
+//     let td5 = document.createElement("td");
+//     td5.innerText = '1234567890';
+//     let td6 = document.createElement("td");
+//     td6.innerHTML = 'aman1234kl';
+//     let td7 = document.createElement("td");
+//     td7.innerHTML = 4.5;
+
+//     tr.append(td1,td2,td3,td4,td5,td6,td7);
+//     tbody.append(tr);
+
+   
+// }
+
+getallusers();
+function getallusers(){
+    // const usertablebody=document.getElementById("tbody");
+    const token="eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJBbWFuIiwic3ViIjoiSldUIFRva2VuIiwidXNlcm5hbWUiOiJkaGFudXNoQGdtYWlsLmNvbSIsImF1dGhvcml0aWVzIjoiUk9MRV9BRE1JTiIsImlhdCI6MTY4NzEwNzA1OSwiZXhwIjoxNjg3MTY3MDU5fQ.3rCI-q82wFE1zyUg4_GIsLl0UAkTWQsVsIwPpWvXWgw";
+
+    fetch("http://localhost:8888/driver/drivers",{
+       method:"GET",
+       headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    }).then(response=>{
+    if(response.ok){
+        response.json().then(data=>{
+            console.log(data);
+            data.forEach(user => {
+                const row=document.createElement("tr");
+                row.innerHTML=`
+                <td>${user.driverId}</td>
+                <td>${user.driverName}</td>
+                <td>${user.email}</td>
+                <td>${user.address}</td>
+                <td>${user.mobileNo}</td>
+                <td>${user.licenceNo}</td>
+                <td>${user.rating}</td>
+                <td><button style="background-color: green;" onclick="updateuser('${user.email}','${user.username}','${user.phone}','${user.address}')">Update</button></td>
+                <td><button style="background-color: red;" onclick="deleteuser('${user.email}')">Delete</button></td>
+                `;
+                tbody.appendChild(row);
+            })
+        })
+    }
+    else{
+        console.log("testing");
+        response.json().then(data=>alert("Sometging went wrong !"));
+    }
+    })
+
+
 }
 
 
-function display(){
-    let tr = document.createElement("tr");
-    let td1 = document.createElement("td");
-    td1.innerHTML = '1';
-    let td2 = document.createElement("td");
-    td2.innerHTML = 'Aman'
-    let td3 = document.createElement("td");
-    td3.innerHTML = 'aman@gmail.com';
-    let td4 = document.createElement("td");
-    td4.innerHTML = 'Kanpur';
-    let td5 = document.createElement("td");
-    td5.innerText = '1234567890';
-    let td6 = document.createElement("td");
-    td6.innerHTML = 'aman1234kl';
-    let td7 = document.createElement("td");
-    td7.innerHTML = 4.5;
 
-    tr.append(td1,td2,td3,td4,td5,td6,td7);
-    tbody.append(tr);
+function deleteuser(mail){
+    const token="eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJBbWFuIiwic3ViIjoiSldUIFRva2VuIiwidXNlcm5hbWUiOiJkaGFudXNoQGdtYWlsLmNvbSIsImF1dGhvcml0aWVzIjoiUk9MRV9BRE1JTiIsImlhdCI6MTY4NzEwNzA1OSwiZXhwIjoxNjg3MTY3MDU5fQ.3rCI-q82wFE1zyUg4_GIsLl0UAkTWQsVsIwPpWvXWgw";
+        
+    // console.log("deleted started");
+    let choice=confirm("Are you sure ? ");
+    if(choice){
+        fetch("http://localhost:8888/driver/"+mail,{
+            method:"DELETE",
+            headers: {
+                'Authorization': `Bearer ${token}`
+              }
+        }
+        ).then(response=>{
+            if(response.ok){
+                response.json().then(data=>{
+                    alert("user deleted successfully !");
+                    location.reload();
+                });
+            }
+            else{
+                response.json().then(data=>alert("Sometging went wrong !"))
+            }
+        })
+    }
 
-   
+}
+
+function updateuser(email,username,phone,address){
+    const url=`Updateuser.html?email=${email}&username=${username}&phone=${phone}&address=${address}`;
+    location.href=url;
 }
