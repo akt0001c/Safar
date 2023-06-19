1d0a6afd654fbba1f101f14b05b6fdaf4b2214b4
akt0001c <110126989+akt0001c@users.noreply.github.com>
2023-06-19 12:56:07 +0530

    update

--- CabManagementSystem/src/main/java/com/safar/controller/WalletController.java
+++ CabManagementSystem/src/main/java/com/safar/controller/WalletController.java
@@ -2,6 +2,7 @@ package com.safar.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
